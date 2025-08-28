1c983c4590e6239cfcddd7098d166897ec82ab87
datadavev <605409+datadavev@users.noreply.github.com>
2025-08-28 10:59:44 -0400

    Update api metadata

--- echo/fapi.py
+++ echo/fapi.py
@@ -1,6 +1,5 @@
 """
 FastAPI application that reflects the request.
-
 """
 
 import json
@@ -9,11 +8,11 @@ import typing
 import fastapi
 import fastapi.middleware.cors
 
-from . import __version__
+from . import __version__, __date__
 
 
 class HumanJsonResponse(fastapi.Response):
-    media_type = "application/json"
+    media_type = 'application/json'
 
     def render(self, content: typing.Any) -> bytes:
         return json.dumps(
@@ -21,34 +20,34 @@ class HumanJsonResponse(fastapi.Response):
             ensure_ascii=False,
             allow_nan=False,
             indent=2,
-            separators=(", ", ": "),
-        ).encode("utf-8")
+            separators=(', ', ': '),
+        ).encode('utf-8')
 
 
 app = fastapi.FastAPI(
-    title="Echo",
-    description=__doc__,
+    title='Echo',
+    description=f'{__doc__}\nDate: {__date__}',
     version=__version__,
     license_info={
-        "name": "GPLv3",
-        "url": "https://www.gnu.org/licenses/gpl-3.0.en.html",
+        'name': 'GPLv3',
+        'url': 'https://www.gnu.org/licenses/gpl-3.0.en.html',
     },
-    openapi_url="/api/v1/openapi.json",
-    docs_url="/api",
+    openapi_url='/api/v1/openapi.json',
+    docs_url='/api',
 )
 
 app.add_middleware(
     fastapi.middleware.cors.CORSMiddleware,
     allow_origins=[
-        "*",
+        '*',
     ],
     allow_credentials=True,
     allow_methods=[
-        "GET",
-        "HEAD",
+        'GET',
+        'HEAD',
     ],
     allow_headers=[
-        "*",
+        '*',
     ],
 )
 
@@ -58,39 +57,39 @@ def request_to_response(
 ) -> typing.Dict[str, typing.Any]:
     headers = {}
     for k, v in request.headers.items():
-        if not k.lower().startswith("x-vercel"):
+        if not k.lower().startswith('x-vercel'):
             headers[k] = v
 
     response = {
-        "url": request.url._url,
-        "method": request.method,
-        "path": path,
-        "query": request.query_params,
-        "headers": headers,
+        'url': request.url._url,
+        'method': request.method,
+        'path': path,
+        'query': request.query_params,
+        'headers': headers,
     }
     return response
 
 
-@app.get("/favicon.ico", include_in_schema=False)
+@app.get('/favicon.ico', include_in_schema=False)
 async def get_favicon():
-    raise fastapi.HTTPException(status_code=404, detail="Not found")
+    raise fastapi.HTTPException(status_code=404, detail='Not found')
 
 
-@app.get("/{path:path}", response_class=HumanJsonResponse)
+@app.get('/{path:path}', response_class=HumanJsonResponse)
 def echo_get(request: fastapi.Request, path: str):
     return request_to_response(request, path)
 
 
-@app.head("/{path:path}", response_class=HumanJsonResponse)
+@app.head('/{path:path}', response_class=HumanJsonResponse)
 def echo_head(request: fastapi.Request, path: str):
     return request_to_response(request, path)
 
 
-@app.post("/{path:path}", response_class=HumanJsonResponse)
+@app.post('/{path:path}', response_class=HumanJsonResponse)
 def echo_post(request: fastapi.Request, path: str):
     return request_to_response(request, path)
 
 
-@app.put("/{path:path}", response_class=HumanJsonResponse)
+@app.put('/{path:path}', response_class=HumanJsonResponse)
 def echo_put(request: fastapi.Request, path: str):
     return request_to_response(request, path)
